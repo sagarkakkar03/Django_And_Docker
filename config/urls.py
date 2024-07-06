c98c18e8b105ea2d9b1b7d1a7b2d54f22f22700d
Sagar <kakkarsagar03.com>
2024-07-06 21:03:05 +0530

    ch5

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
+    
+    # User Management 
+    path('accounts/', include('django.contrib.auth.urls')),
+    
+    # Local apps 
+    path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
+
 ]

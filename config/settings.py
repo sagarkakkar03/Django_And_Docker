8dceae889252a1da65e89ec0f3da5848783f02a9
Sagar <kakkarsagar03.com>
2024-07-15 14:27:12 +0530

    ch6

--- config/settings.py
+++ config/settings.py
@@ -37,12 +37,18 @@ INSTALLED_APPS = [
 'django.contrib.sessions',
 'django.contrib.messages',
 'django.contrib.staticfiles',
+
+# Third-party
+'crispy_forms',
+
 # local
 'accounts', 
 'pages',
 
 ]
 
+# django-crispy-forms 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 AUTH_USER_MODEL = 'accounts.CustomUser' # new
 
@@ -125,9 +131,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (BASE_DIR/'static',)
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
-STATIC_URL = 'static/'
-
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse , resolve
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 # Create your tests here.
 
 class HomepageTests(SimpleTestCase):
@@ -27,3 +27,29 @@ class HomepageTests(SimpleTestCase):
             view.func.__name__,
             HomePageView.as_view().__name__
         )
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+    def test_about_page_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+    
+    def test_about_page_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+    
+    def test_about_page__does_not_contains_incorrect_html(self):
+        self.assertNotContains(
+            self.response, 'Hi there! I should not be on the page'
+        )
+
+    def test_about_page_contains_correct_html(self):
+        self.assertContains(self.response, 'About Page')
+    
+    def test_about_page_url_resolves_about_page_view(self):
+        view = resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
+        )
+    
